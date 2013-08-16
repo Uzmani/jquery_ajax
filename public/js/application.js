7ffdc863f844495051338531202a9845cf7c8cb5
uzmani <uzmani@yahoo.com>
2013-08-15 21:47:12 -0700

    first commit

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
+  $('#get_color').on('click', function(event) {
+    console.log('im clicking click me');
 
+    event.preventDefault();
+    
+    $.ajax({
+    type: 'post',
+    url:'/color'
+    }).done(function(server_data) {
+      console.log(server_data.cell_color);
+      console.log(server_data.box);
+       $('#color_me :nth-child('+server_data.box+')').css('background-color', server_data.cell_color)
+    });
+
+
+  });
 });
